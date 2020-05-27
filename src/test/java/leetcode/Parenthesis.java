c38ddb0c4d763afbaac3801b9af706b9b53a355e
Carlos Chacin <cchacin@gmail.com>
2020-05-27 04:08:44 -0700

    Fix Parenthesis

--- src/test/java/leetcode/Parenthesis.java
+++ src/test/java/leetcode/Parenthesis.java
@@ -8,50 +8,72 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Stack;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.params.provider.Arguments.*;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
+/**
+ * https://leetcode.com/problems/valid-parentheses/
+ * <p>
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+ * determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: "(]"
+ * Output: false
+ * Example 4:
+ * <p>
+ * Input: "([)]"
+ * Output: false
+ * Example 5:
+ * <p>
+ * Input: "{[]}"
+ * Output: true
+ */
 class Parenthesis implements WithAssertions {
 
     static Stream<Arguments> arguments() {
         return Stream.of(
-                of("((()))", "((()))"),
-                of("((()))()", "((()))()")
+                of("((()))", true),
+                of("((()))()", true),
+                of("((()))(]", false)
         );
     }
 
-    @ParameterizedTest(name = "balanced({0}) should return {1}")
+    @ParameterizedTest(name = "isValid({0}) should return {1}")
     @MethodSource("arguments")
     void test(
             String input,
-            String expected) {
-        assertThat(balanced(input)).isEqualTo(expected);
+            boolean expected) {
+        assertThat(isValid(input)).isEqualTo(expected);
     }
 
-    // TODO incomplete
-    String balanced(
+    boolean isValid(
             String input) {
-        StringBuilder result = new StringBuilder();
-
-        Stack<Character> stack = new Stack<>();
-        char[] chars = input.toCharArray();
-
-        for (char aChar : chars) {
-            if (aChar == '(') {
-                stack.push(aChar);
-                result.append(aChar);
-            }
-            if (aChar == ')') { // aChar == ')' && open > 0
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    result.append(aChar);
-                    stack.pop();
-//                    if (!stack.isEmpty()) {
-//                        result.append(aChar);
-//                    }
-                } else {
-                    stack.push(aChar);
-                }
-            }
+        var stack = new Stack<Character>();
+        for (var c : input.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
         }
-        return result.toString();
+        return stack.isEmpty();
     }
 }
